91f7dfec14c646130520596d1626f20e74cfd9c3
Leodarkseid <leodarkseid@gmail.com>
2022-06-16 17:44:05 +0100

    add host

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['genquizapp.herokuapp.com']
+ALLOWED_HOSTS = ['genquizapp.herokuapp.com','www.badmuskolade.com','badmuskolade.com']
 
 
 # Application definition
